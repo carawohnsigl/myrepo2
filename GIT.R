389eeea36686c92eef2ca7d71ea94376f5a2f080
carawohnsigl <70346026+carawohnsigl@users.noreply.github.com>
2024-02-02 09:59:08 -0500

    Add R scripts

--- GIT.R
+++ GIT.R
@@ -1,2 +1,22 @@
 GIT TEST fsdjafas
 test
+system("git init")
+system("git add .")
+system("git commit -m 'Initial commit'")
+system("git remote add origin git@github.com:carawohnsigl/myrepo2.git")
+system("git push -u origin master")
+system("git remote -v")
+
+system("git remote set-url origin git@github.com:carawohnsigl/myrepo2.git")
+
+system("git push - u origin master")
+system("git init")
+system("git add .")
+system("git commit -m 'Initial commit'")
+system("git push -u origin master")
+
+# Remove the existing "origin" remote
+system("git remote remove origin")
+
+# Add the remote with the correct URL
+system("git remote add origin git@github.com:carawohnsigl/myrepo2.git")
